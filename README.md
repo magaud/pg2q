60d6b0cb6c7ab2fec1c99f1fad085f3058a2bccb
magaud <magaud@unistra.fr>
2026-03-12 20:34:48 +0100

    more steps

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+* (5.3.0)rocq 9.0.0 and rocq-mathcomp-algebra 2.4.0
+
 * Models of projective plane geometry
 In pg(2,q), there are (q^2+q+1) points, and also (q^2+q+1) lines. Each line contains (q+1) points, and dually each point is incident to exactly (q+1) lines. 
 
@@ -6,4 +8,5 @@ In pg(2,q), there are (q^2+q+1) points, and also (q^2+q+1) lines. Each line cont
  + in AISC 2018, Desargues is proved in pg(2,2) and pg(2,3)...
  + find counterexamples of Desargues theorem in Hall, Dual Hall or Hugues projective planes
  + show that the 4 planes of order 9 are not isomorphic (collineations in order 9)
+ + is Desargues stable by collineations ?
  
---
+++ bruck_ryser.v
@@ -0,0 +1,124 @@
+From HB Require Import structures.
+From mathcomp Require Import all_ssreflect perm fingroup.
+From PG2X Require Import projective_plane_axioms_alt order incid_mats.
+From Stdlib Require Import Arith. 
+
+Section bruck_ryser.
+
+Context
+  {point : finType}
+  {line : finType}
+  {incid : point -> line -> bool}
+  {PP : ProjectivePlane point line incid}.
+
+Theorem bruck_ryser : 
+  forall (n:nat), ( order = n) -> Nat.modulo n 4 = 1 \/ Nat.modulo n 4 = 2 -> 
+exists p:nat, exists q:nat, (n = p * p + q * q)%nat.
+Proof.
+Admitted.
+
+
+Lemma bruck_ryser' : forall n:nat,(Nat.modulo n 4 = 1 \/ Nat.modulo n 4 = 2) -> 
+(forall p q:nat, n != p * p + q * q) -> ~exists i:incid_struct, is_pp i /\ (forall H:(is_pp i), order i H=n).
+Proof.
+intros n Hmod Hsum.
+intro Hneg. 
+destruct Hneg as [i [Hi Ho]].
+destruct (bruck_ryser i n Hi Ho Hmod) as [p [q Hpq]].
+generalize (Hsum p q); clear Hsum; intros Hsum.
+rewrite <- Hpq in Hsum.
+by rewrite eqxx in Hsum.
+Qed.
+
+Lemma foo_bar_6 : forall p q:nat, 6<> p * p + q * q.
+Proof.
+intros; intro.
+
+destruct p.
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+
+destruct p.
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+-
+destruct p.
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+Qed.
+
+Lemma foo_6 :  forall p q:nat, 6 != p * p + q * q.
+Proof.
+intros; apply/eqP.
+apply foo_bar_6.
+Qed.
+
+Lemma no_plane_exists_when_n_is_6 : ~exists i:incid_struct, is_pp i /\ forall H:is_pp i, order i H = 6.
+Proof.
+apply bruck_ryser'.
+right; reflexivity.
+apply foo_6.
+Qed.
+
+Lemma foo_bar_14 : forall p q:nat, 14<> p * p + q * q.
+Proof.
+intros; intro.
+
+destruct p.
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+
+destruct p.
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+
+destruct p.
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+
+destruct p.
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+destruct q; [ discriminate | idtac].
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+
+
+cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; cbv in H; repeat rewrite <- Nat.add_succ_comm in H; discriminate.
+Qed.
+
+Lemma foo_14 :  forall p q:nat, 14 != p * p + q * q.
+Proof.
+intros; apply/eqP.
+apply foo_bar_14.
+Qed.
+
+Lemma no_plane_exists_when_n_is_14 :  ~exists i:incid_struct, is_pp i /\ forall H:is_pp i, order i H = 14.
+Proof.
+apply bruck_ryser'.
+right; reflexivity.
+apply foo_14.
+Qed.
+
+(* 2.2.1 proves that there exists a finite projective plane of order n whether n is a prime power *)
+
+(* 3.3.1 Bruck-Ryser rules out several other open cases *)
+(* orders not ruled out by 3.3.1 are 10, 12, 15, 18, 20, 24, 26, 28, 34, 35, 36, 39, 40, 44, 45, 48, 50, 51, 52, 55, 56, 58, 60, 63, 65, 68, 72, 74, 75, 76, 80, 82, 84, 85, 87, 88, 90, 91, 92, 95, 96, 98, 99 and 100 *)
+
